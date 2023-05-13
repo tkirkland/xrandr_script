d8d5471c54a1f91831e58c726b68928d39b8458d
Tony Kirkland <tony@tonykirkland.net>
2023-05-13 02:25:23 -0400

    added comment to explain that which looks unneeded

--- scaleexternalsplay.zsh
+++ scaleexternalsplay.zsh
@@ -2,4 +2,6 @@
 
 xrandr --output "DP-4" --mode "3840x2160" --rate "120" --scale "1x1" --output "DP-2" --mode "2560x1440" --rate "165" --scale "1.5x1.5" --right-of "DP-4" --pos "3840x360" --primary
 
+# Works in tandem with the previous --primary to force Plasma to resize wallpaper to to scaled layout
+# without forcing a resolution changr
 xrandr --output "DP-4" --primary
