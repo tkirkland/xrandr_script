bef5a3ea2062730bda13dc5bbf8810a45be1bd08
Tony Kirkland <tony@tonykirkland.net>
2023-05-13 02:08:00 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# XRANDR Config
+
+A simple shell script that initializes screens on my Dell laptop running Linux
+
+## Version History
+
+* 0.1
+    * First attempt
---
+++ scaleexternalsplay.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+xrandr --output "DP-4" --mode "3840x2160" --rate "120" --scale "1x1" --output "DP-2" --mode "2560x1440" --rate "165" --scale "1.5x1.5" --right-of "DP-4" --pos "3840x360" --primary
+
+xrandr --output "DP-4" --primary
