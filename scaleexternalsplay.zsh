8529648cc1b0b04a704b0c573019eec7977ee7b4
Tony <tony@tonykirkland.net>
2023-09-03 06:29:48 -0400

    qodana

--- scaleexternalsplay.zsh
+++ scaleexternalsplay.zsh
@@ -1,7 +1,8 @@
-#!/usr/bin/env zsh
+#!/bin/bash
 
-xrandr --output "DP-4" --mode "3840x2160" --rate "120" --scale "1x1" --output "DP-2" --mode "2560x1440" --rate "165" --scale "1.5x1.5" --right-of "DP-4" --pos "3840x360" --primary
+xrandr --output "DP-4" --mode "3840x2160" --rate "120" --scale "1x1" --output "DP-2" --mode "2560x1440" --rate "165" \
+--scale "1.5x1.5" --right-of "DP-4" --pos "3840x360" --primary
 
 # Works in tandem with the previous --primary to force Plasma to resize wallpaper to scaled layout desktop size
-# without forcing a resolution changr
+# without forcing a resolution changer
 xrandr --output "DP-4" --primary
